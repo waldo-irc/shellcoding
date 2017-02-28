8bd83b7c4bdb9af502efc7cba75b4a4bf60dbec3
root <root@localhost.localdomain>
2017-02-27 22:35:22 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo 'Need a valid PATH to install to.'
+    exit 0
+fi
+
+cp chex "$1"
+cp hex "$1"
+cp stringhex "$1"
+cp dump "$1"
+cp plt "$1"
+cp newasm "$1"
+
+read -p "Install to $1? [y/N]: " CONTINUE;
+if [ "$CONTINUE" == "Y" ] | [ "$CONTINUE" == "y" ]; then
+    echo "Installing to $1"
+else
+    echo "exiting"
+    exit 0
+fi
