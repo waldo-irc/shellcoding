15df2593e8bd8ef444849877d50f122402206fea
waldo-irc <axparsa@gmail.com>
2017-02-27 23:46:25 -0500

    installer fix

--- install.sh
+++ install.sh
@@ -4,16 +4,15 @@ if [ -z "$1" ]; then
     exit 0
 fi
 
-cp chex "$1"
-cp hex "$1"
-cp stringhex "$1"
-cp dump "$1"
-cp plt "$1"
-cp newasm "$1"
-
 read -p "Install to $1? [y/N]: " CONTINUE;
 if [ "$CONTINUE" == "Y" ] | [ "$CONTINUE" == "y" ]; then
     echo "Installing to $1"
+    cp chex "$1"
+    cp hex "$1"
+    cp stringhex "$1"
+    cp dump "$1"
+    cp plt "$1"
+    cp newasm "$1"
 else
     echo "exiting"
     exit 0
